51b626f3a6b6a8dc1ab6035e18fd57baab8db695
FengL975365262 <34015652+FengL975365262@users.noreply.github.com>
2018-02-13 20:50:58 +0800

    userREG
    写了一些userreg方法

--- src/Service/UserReg.java
+++ src/Service/UserReg.java
@@ -1,5 +1,26 @@
 package Service;
 
-public class UserReg {
-	
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserReg extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		String username=req.getParameter("username");
+		String useridno=req.getParameter("useridno");
+		String userphone=req.getParameter("userphone");
+		String useraddr=req.getParameter("useraddr");
+		String usermail=req.getParameter("usermail");
+		String userpsw=req.getParameter("userpsw");
+	}
 }
